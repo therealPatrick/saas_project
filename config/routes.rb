e2cb74cc155ad1329e77d320d518bf7625e39d04
therealPatrick <malamashangala13@gmail.com>
2024-07-20 14:59:18 +0200

    clean up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 	get 'admin/dashboard', to: 'page#dashboard'
 	get 'pricing', to: 'page#pricing'
-	get 'about', to: 'page#about'
+	# get 'about', to: 'page#about'
   if Rails.env.development? || Rails.env.test?
     mount Railsui::Engine, at: "/railsui"
   end
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   # Inherits from Railsui::PageController#index
   # To overide, add your own page#index view or change to a new root
   # Visit the start page for Rails UI any time at /railsui/start
-  root action: :index, controller: "railsui/page"
+  # root action: :index, controller: "railsui/page"
+  root action: 'about', to: 'page#about'
 
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
