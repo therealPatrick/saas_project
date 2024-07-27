878083e8246ae723bbe6be073673d08e4606b511
therealPatrick <malamashangala13@gmail.com>
2024-07-27 22:15:14 +0200

    routing to dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resource)
+    admin_dashboard_path
+  end
+
   helper Railsui::ThemeHelper
 
   before_action :configure_permitted_parameters, if: :devise_controller?
